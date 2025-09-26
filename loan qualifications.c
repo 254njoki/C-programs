fcb95cb50b106abc3601752d4983125a7095e5b8
254njoki <njokifidelis12@gmail.com>
2025-09-27 00:48:43 +0300

    Add files via upload

---
+++ loan qualifications.c
@@ -0,0 +1,27 @@
+/*
+Author: Fidelis Njoki
+Reg no:CT100/G/26151/25
+Date:23/09/2025
+Description: Bank loan
+*/
+//pre processor directive 
+#include<stdio.h>//scanf(),printf()
+
+int main(){
+float income;
+
+//variable declaration& initialization 
+int age;
+printf("Enter your age:\n");
+scanf("%d",&age);
+
+printf("Enter your income:\n");
+scanf("%d",&income);
+
+
+if("age >= 21 && income>=21000:\n"){
+printf("congratulations you qualify for a loan");}
+else{
+printf("unfortunately,are unable to offer you a loan");}
+return 0;
+}

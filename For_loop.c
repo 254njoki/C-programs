7ad9eb9aed6e5c10f2e9305bb7760892e41e6b40
254njoki <njokifidelis12@gmail.com>
2025-10-05 23:16:52 +0300

    Add files via upload

---
+++ For_loop.c
@@ -0,0 +1,20 @@
+/*
+Author: Fidelis Njoki
+Reg no:CT100/G/26151/25
+Date:4/10/2025
+Description: For_loop
+*/
+#include<stdio.h>//scantf(),printf()
+//main function 
+
+int main(){
+int d;
+//for(start;stop;step)
+//for(initilisation;condition;decrement)
+
+for(d=100;d>=50;d--){
+printf("%d\n",d);
+}
+return 0;
+
+}
